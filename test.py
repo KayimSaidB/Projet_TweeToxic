71239d84c251986b7b91e98875e1eaca8bec176e
KayimSaidB <kayimsaid@hotmail.fr>
2019-12-06 14:22:18 +0100

    Premier test de connexion à l'API Twi

---
+++ test.py
@@ -0,0 +1,28 @@
+
+import tweepy
+
+access_token="3239543231-CiRmGhEUaYfSSSlRAortBS22KvffmQ7KzxsIeC4"
+
+access_token_secret="dNICY8SGaUCkoTiRwFflwFU5lcCB8h4ghTPG3r6Xy7arT"
+consumer_key="L6KBC06aWFGM3AhwWZxmtaXar"
+consumer_secret="bVjl53P1DRNGr9hm4Wa5VjwFSevaL31cmv9V7HEhHykORi6jIm"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+i=0
+for tweet in tweepy.Cursor(api.user_timeline,id='kayiminus').items():
+   if i ==10:
+   	break 
+   if(tweet.text[0]!='@' and tweet.text[0:2]!='RT'):
+   	print (tweet.text)
+   	i=i+1
+
+
+
+#user = api.get_user('NonsR971')
